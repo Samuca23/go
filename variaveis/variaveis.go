2e24c486b3b2d5bef628d1c58a517ec9bdab85d5
Samuel <samuel.chiodini@unidavi.edu.br>
2024-02-02 08:37:51 -0300

    variaveis

---
+++ variaveis/variaveis.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	nome := "Samuel"
+	versao := 1.1
+	idade := 23
+
+	fmt.Println("Olá, Sr.", nome, "sua idade é", idade)
+	fmt.Println("Este programa está na versão", versao)
+}
